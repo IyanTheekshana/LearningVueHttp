7cc75a4d8611c744759aba3b2bad718d02947d6f
Iyan Theekshana <iyan.dev123@gmail.com>
2023-03-06 12:14:11 +0100

    fetch data and implemented POST and GET methods

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,69 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from "./SurveyResult.vue";
+
+export default {
+  // props: ['results'],
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        "https://vue-http-demo-4c5ef-default-rtdb.europe-west1.firebasedatabase.app/surveys.json"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          const resultsArray = [];
+          for (const id in data) {
+            resultsArray.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+
+          this.results = resultsArray;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
